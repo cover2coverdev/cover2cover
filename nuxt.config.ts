5aaabbe44f509ede779724b9cc65de343a9baeab
Matt <reidmt.21@gmail.com>
2024-02-13 13:02:13 -0500

    added daisy ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,16 @@
+import daisyui from 'daisyui'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   nitro: {
-    preset: 'vercel'
+    preset: "vercel",
   },
   devtools: { enabled: true },
-  modules: ["@nuxtjs/eslint-module", "@nuxt/test-utils/module", 'nuxt-icon', '@unocss/nuxt'],
-  eslint: {
-    
+  modules: ["@nuxtjs/eslint-module", "@nuxt/test-utils/module", "nuxt-icon", "@nuxtjs/tailwindcss"],
+  eslint: {},
+  tailwindcss: {
+    config: {
+      plugins: [daisyui],
+    },
   },
   build: {
     transpile: ["trpc-nuxt"],
