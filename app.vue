3d39684667f843ac9193810ef51386ff55a76b1b
Matt <reidmt.21@gmail.com>
2024-02-13 11:44:43 -0500

    added uno cause css bad

--- app.vue
+++ app.vue
@@ -2,13 +2,11 @@
 const { $trpcClient } = useNuxtApp()
 
 //TODO: add eslint config
-const { data } = await $trpcClient.users.useQuery()
 
 </script>
 
 <template>
   <div>
-    <p v-if="data?.users.length === 0">No users</p>
-    <Hello />
+    <Hello/>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: ['~/assets/css/reset.css', '~/assets/css/main.css'],
   nitro: {
     preset: 'vercel'
   },
   devtools: { enabled: true },
-  modules: ["@nuxt/test-utils/module"],
+  modules: ["@nuxt/test-utils/module", 'nuxt-icon', '@unocss/nuxt'],
   build: {
     transpile: ["trpc-nuxt"],
   },
