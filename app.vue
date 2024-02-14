4f095702adeafc8b05bbe5b8c9f1e7c552eaa896
Matt <reidmt.21@gmail.com>
2024-02-14 17:56:11 -0500

    add nuxt color theme, tailwind typography

--- app.vue
+++ app.vue
@@ -1,64 +1,6 @@
-<script setup lang="ts">
-import type { GoogleBookVolume } from "./types/googlebooks";
-
-const { $trpcClient } = useNuxtApp();
-useHead({ htmlAttrs: { "data-theme": "pastel" } });
-const query = ref("");
-const volumes = ref<GoogleBookVolume[]>([]);
-
-const handleSearch = async () => {
-  if (!query.value.length) return;
-  const res = await $trpcClient.googlebooks.query({ q: query.value, maxResults: 12 });
-  volumes.value = res.items;
-};
-</script>
 
 <template>
-  <div class="hero min-h-screen bg-base-200">
-    <ClientOnly>
-      <div class="hero-content text-center">
-        <div class="max-w-md">
-          <Transition
-            enter-active-class="duration-500 ease-out"
-            enter-from-class="opacity-0 translate-y-[-48px]"
-            enter-to-class="opacity-100 translate-y-0"
-            leave-active-class="duration-500 ease-in"
-            leave-from-class="opacity-100"
-            leave-to-class="opacity-0 translate-y-[-48px]"
-            appear
-          >
-            <h1 class="text-5xl font-bold">
-              Welcome to <br>
-              cover 2 cover
-            </h1>
-          </Transition>
-
-          <div class="flex flex-col gap-2 items-center">
-            <p class="py-6">
-              We have tons of exciting <Spoiler> things </Spoiler> to <Spoiler>show</Spoiler> you soon. We hope you will
-              like our app. We present to you <Spoiler>cover2cover</Spoiler>
-            </p>
-            <div class="flex items-center gap-2">
-              <input
-                v-model="query"
-                type="text"
-                placeholder="Type here"
-                class="input input-bordered w-[300px]"
-              >
-              <button
-                class="btn btn-neutral"
-                @click="handleSearch"
-              >
-                find
-              </button>
-            </div>
-            <BookList
-              v-if="volumes.length > 0"
-              :volumes="volumes"
-            />
-          </div>
-        </div>
-      </div>
-    </ClientOnly>
+  <div>
+    <NuxtPage />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 import daisyui from "daisyui";
+import tailwindTypography from '@tailwindcss/typography'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   nitro: {
@@ -8,13 +9,17 @@ export default defineNuxtConfig({
     GOOGLE_BOOKS_API_KEY: process.env.GOOGLE_BOOKS_API_KEY
   },
   devtools: { enabled: true },
-  modules: ["@nuxtjs/eslint-module", "@nuxt/test-utils/module", "nuxt-icon", "@nuxtjs/tailwindcss"],
-  eslint: {},
+  modules: ["@nuxtjs/eslint-module", "@nuxt/test-utils/module", "nuxt-icon", "@nuxtjs/tailwindcss", '@nuxtjs/color-mode'],
+  colorMode: {
+    dataValue: 'theme',
+    preference: 'pastel',
+    fallback: 'dark'
+  },
   tailwindcss: {
     cssPath: '~/assets/css/tailwind.css',
     exposeConfig: false,
     config: {
-      plugins: [daisyui],
+      plugins: [tailwindTypography, daisyui],
       daisyui: {
         logs: false,
         themes: [
